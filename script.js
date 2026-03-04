5f4fd932c3e767625207df4591de52231bb6b433
mrGarshin <garshin.serg@yandex.ru>
2026-03-04 15:07:32 +0300

    add playGame func logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0
 let computerScore = 0
+let draw = 0
 
 const getComputerChoice = function() {
     const result = Math.random() * 10
@@ -12,14 +13,34 @@ const getHumanChoice = function() {
     let choice = prompt("What's your choice?")
     return choice.toLowerCase()
 }
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
 
 const playRound = function(humanChoice, computerChoice) {
     
-    if(humanChoice == computerChoice) return 'draw'
+    if(humanChoice == computerChoice) draw += 1
     if((humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'scissors' && computerChoice == 'paper')) humanScore += 1
     if((computerChoice == 'rock' && humanChoice == 'scissors') || (computerChoice == 'paper' && humanChoice == 'rock') || (computerChoice == 'scissors' && humanChoice == 'paper')) computerScore += 1
-    console.log('humanScore = ' + humanScore + '; computerScore = ' + computerScore)
+    
 }
 
+
+
+
+const playGame = function(repetition) {
+    let humanSelection = getHumanChoice()
+    let computerSelection = getComputerChoice()
+    for (let i = 1; i < repetition+1; i++) {
+        playRound(humanSelection, computerSelection)
+        
+        console.log("round - " + i+ '. humanScore = ' + humanScore + '; computerScore = ' + computerScore + '; draw - ' + draw)
+        if (i < repetition) {
+            humanSelection = getHumanChoice()
+            computerSelection = getComputerChoice()
+        }
+        
+    }
+    
+    console.log('Final scores: humanScore = ' + humanScore + '; computerScore = ' + computerScore)
+    
+}
+
+playGame(3)
